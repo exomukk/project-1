5a6e3132ed6fd69ddfd339602962bc6afdc5c6e5
exomuck <tranhuy.dev88@gmail.com>
2024-05-19 23:58:14 +0700

    update frontend - still bug when loggin

--- src/demo/src/main/resources/static/assets/js/login.js
+++ src/demo/src/main/resources/static/assets/js/login.js
@@ -1,29 +1,3 @@
-$(document).ready(function () {
-    $(".container").fadeIn(1000);
-    $(".s2class").css({ "color": "#EE9BA3" });
-    $(".s1class").css({ "color": "#748194" });
-    $("#left").removeClass("left_hover");
-    $("#right").addClass("right_hover");
-    $(".signin").css({ "display": "none" });
-    $(".signup").css({ "display": "" });
-});
-$("#right").click(function () {
-    $("#left").removeClass("left_hover");
-    $(".s2class").css({ "color": "#EE9BA3" });
-    $(".s1class").css({ "color": "#748194" });
-    $("#right").addClass("right_hover");
-    $(".signin").css({ "display": "none" });
-    $(".signup").css({ "display": "" });
-});
-$("#left").click(function () {
-    $(".s1class").css({ "color": "#EE9BA3" });
-    $(".s2class").css({ "color": "#748194" });
-    $("#right").removeClass("right_hover");
-    $("#left").addClass("left_hover");
-    $(".signup").css({ "display": "none" });
-    $(".signin").css({ "display": "" });
-});
-
 function submitForm() {
     var username = document.getElementsByClassName("username")[0].value;
     var password = document.getElementsByClassName("password")[0].value;
